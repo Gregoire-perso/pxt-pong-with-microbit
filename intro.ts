a84b110b326fe69824c2772f7789f5a4db6a95bd
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2019-10-20 16:34:22 +0200

    Premier test

---
+++ intro.ts
@@ -0,0 +1,4 @@
+function intro() {
+    LCD1IN8.DisString(0, 0, "Coucou les gens ! C'est un test d'intro parce qu'en vrai on en fera une mieux mais c'est pas mon domaine, celui de Max", 0)
+    LCD1IN8.LCD_DisplayWindows(0, 0, 0, 0)
+}
--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_Clear()
+LCD1IN8.DisString(0, 0, "A | a | B | b | C | c", 0)
+intro() 
