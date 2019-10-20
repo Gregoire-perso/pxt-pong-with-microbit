420150261fff009fe9de3f8482c07106c1fb4308
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2019-10-20 17:48:04 +0200

    Différents tests

--- intro.ts
+++ intro.ts
@@ -1,4 +1,6 @@
 function intro() {
-    LCD1IN8.DisString(0, 0, "Coucou les gens ! C'est un test d'intro parce qu'en vrai on en fera une mieux mais c'est pas mon domaine, celui de Max", 0)
-    LCD1IN8.LCD_DisplayWindows(0, 0, 0, 0)
+    let a = "Coucou les gens ! C'est un test d'intro parce qu'en vrai on en fera une mieux mais c'est pas mon domaine, celui de Max"
+    LCD1IN8.DisString(0, 0, a, 0)
+    LCD1IN8.LCD_Display()
+    //LCD1IN8.LCD_DisplayWindows(0, 0, a.length * 7 + 20, 20)
 }
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_Clear()
-LCD1IN8.DisString(0, 0, "A | a | B | b | C | c", 0)
-intro() 
+intro()
+
