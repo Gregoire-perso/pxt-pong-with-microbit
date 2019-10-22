8e27d83b030b968fa5e88bd0442b684bece6bc95
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2019-10-22 16:48:41 +0200

    Ajustement mouvement barre

--- barre.ts
+++ barre.ts
@@ -1,22 +1,26 @@
 // Ajouter votre code ici
 let x = 70;
 let y = 115;
-
+let largeur_barre = 25 - 1; //
 
 input.onButtonPressed(Button.AB, function () {
-    LCD1IN8.DrawRectangle(x, y, x + 20, y + 5, 64512, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
-    LCD1IN8.LCD_DisplayWindows(x - 1, y - 1, x + 20, y + 4)
-})
-input.onButtonPressed(Button.A, function () {
-    LCD1IN8.DrawRectangle(x, y, x + 20, y + 5, LCD_COLOR.WHITE, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
-    x -= 2
-    LCD1IN8.DrawRectangle(x, y, x + 20, y + 5, 64512, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
-    LCD1IN8.LCD_DisplayWindows(x - 1, y - 1, x + 22, y + 4)
+    LCD1IN8.LCD_Clear()
+    LCD1IN8.DrawRectangle(x, y, x + largeur_barre, y + 5, 64512, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
+    LCD1IN8.LCD_DisplayWindows(x - 1, y - 1, x + largeur_barre, y + 4)
 })
 
-input.onButtonPressed(Button.B, function () {
-    LCD1IN8.DrawRectangle(x, y, x + 20, y + 5, LCD_COLOR.WHITE, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
-    x += 2
-    LCD1IN8.DrawRectangle(x, y, x + 20, y + 5, 64512, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
-    LCD1IN8.LCD_DisplayWindows(x - 3, y - 1, x + 20, y + 4)
-})
+while (true) {
+    while (input.buttonIsPressed(Button.A) && x >= 2) {
+        LCD1IN8.DrawRectangle(x, y, x + largeur_barre, y + 5, LCD_COLOR.WHITE, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
+        x -= 1
+        LCD1IN8.DrawRectangle(x, y, x + largeur_barre, y + 5, 64512, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
+        LCD1IN8.LCD_DisplayWindows(x - 1, y - 1, x + largeur_barre + 2, y + 4)
+    }
+
+    while (input.buttonIsPressed(Button.B) && (x + largeur_barre < 160)) {
+        LCD1IN8.DrawRectangle(x, y, x + largeur_barre, y + 5, LCD_COLOR.WHITE, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
+        x += 1
+        LCD1IN8.DrawRectangle(x, y, x + largeur_barre, y + 5, 64512, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
+        LCD1IN8.LCD_DisplayWindows(x - 3, y - 1, x + largeur_barre, y + 4)
+    }
+}
