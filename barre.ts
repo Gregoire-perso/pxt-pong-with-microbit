8dc0877e992fa89e51932b19421bbcf1ad39e477
Grégoire <36741392+Gregoire-perso@users.noreply.github.com>
2019-10-22 16:53:21 +0200

    Précisions mouvement barre

--- barre.ts
+++ barre.ts
@@ -1,10 +1,13 @@
-// Ajouter votre code ici
+basic.pause(5000)
+
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_Clear()
+
+let largeur_barre = 25 - 1; //La largeur effective de la barre est ici 25
 let x = 70;
 let y = 115;
-let largeur_barre = 25 - 1; //
 
 input.onButtonPressed(Button.AB, function () {
-    LCD1IN8.LCD_Clear()
     LCD1IN8.DrawRectangle(x, y, x + largeur_barre, y + 5, 64512, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1)
     LCD1IN8.LCD_DisplayWindows(x - 1, y - 1, x + largeur_barre, y + 4)
 })
--- main.ts
+++ main.ts
@@ -1,2 +1,4 @@
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_Clear()
+
+LCD1IN8.LCD_Filling(0)
